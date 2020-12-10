00052d0eed67d76546fa3fce55e52209cccfa323
Adsekar04 <adsekar@ucsc.edu>
2020-12-09 18:00:30 -0800

    Added some GET tests

--- puttest.sh
+++ puttest.sh
@@ -1,4 +1,6 @@
 putTest(){
+	echo "============================================"
+	echo "Testing PUT\n\n"
 	#First testing errors
 	echo "Testing missing key"
 	curl --request   PUT --header    "Content-Type: application/json" --write-out "%{http_code}\n" --data '{"causal-context":{}}' http://127.0.0.1:13802/kvs/keys/sampleKey
@@ -34,19 +36,66 @@ putTest(){
        --write-out "%{http_code}\n"                                                 \
        --data      '{"value":"new sampleValue_2","causal-context":{}}' \
        http://127.0.0.1:13802/kvs/keys/sampleKey_2
-    #Now going to test PUT's with causal-context, emulating concurrent requests
+}
+
+getTest(){
+	echo "============================================"
+	echo "Testing GET\n\n"
+	#First testing for invalid GET's
+	echo "Testing missing key GET"
+	curl --request   GET                                        \
+       --header    "Content-Type: application/json"           \
+       --write-out "%{http_code}\n"                           \
+       --data      '{"causal-context":{}}' \
+       http://127.0.0.1:13800/kvs/keys/missingKey
+
+    #Testing Valid GET
+    echo "Testing Valid GET for existing key"
+    curl --request   GET                                        \
+       --header    "Content-Type: application/json"           \
+       --write-out "%{http_code}\n"                           \
+       --data      '{"causal-context":{}}' \
+       http://127.0.0.1:13800/kvs/keys/sampleKey
+
+}
+
+#Tests concurrent PUTS, and PUTS and GETS together to check if we get expected values
+putGetTest(){
+	echo "============================================"
+	echo "PUT and GET together\n\n"
+	#Now going to test PUT's with causal-context, emulating concurrent requests
     echo "Testing Valid PUT, with equal VC as end nodes VC"
     curl --request   PUT                                                              \
        --header    "Content-Type: application/json"                                 \
        --write-out "%{http_code}\n"                                                 \
-       --data      '{"value":"change sampleValue","causal-context":{"sampleKey": 2}}' \
+       --data      '{"value":"changed sampleValue","causal-context":{"sampleKey": 2}}' \
        http://127.0.0.1:13802/kvs/keys/sampleKey
+    echo "Testing GET, to see what value is there for sampleKey"
+    curl --request   GET                                        \
+       --header    "Content-Type: application/json"           \
+       --write-out "%{http_code}\n"                           \
+       --data      '{"causal-context":{}}' \
+       http://127.0.0.1:13800/kvs/keys/sampleKey
     echo "Testing Valid PUT with causal-context, with VC less than end nodes VC"
     curl --request   PUT                                                              \
        --header    "Content-Type: application/json"                                 \
        --write-out "%{http_code}\n"                                                 \
-       --data      '{"value":"change sampleValue","causal-context":{"sampleKey": 1}}' \
+       --data      '{"value":"wrong sampleValue","causal-context":{"sampleKey": 1}}' \
        http://127.0.0.1:13802/kvs/keys/sampleKey
+    echo "Testing GET, to see what value is there for sampleKey"
+    curl --request   GET                                        \
+       --header    "Content-Type: application/json"           \
+       --write-out "%{http_code}\n"                           \
+       --data      '{"causal-context":{}}' \
+       http://127.0.0.1:13800/kvs/keys/sampleKey
 }
 
-putTest
+
+
+test(){
+	putTest
+	getTest
+	putGetTest
+}
+
+test
